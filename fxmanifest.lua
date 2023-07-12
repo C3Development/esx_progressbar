1249120d60924b04e665db8b1369ec3357e8c98f
C3h1cDevelopment <128829862+C3-FrameWork@users.noreply.github.com>
2023-07-12 19:21:27 +0200

    1.0 - RELEASE

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Its_Cehic'
+version '1.0'
+description 'Circle Progress Bar - Test Version'
+-- Created and Edited by Its_Cehic
+
+client_script 'nui/script.js'
+server_script 'progress.lua'
+
+ui_page 'nui/index.html'
+
+files {
+  'nui/index.html',
+  'nui/styles.css'
+}
